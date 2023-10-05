de9c1aebd7a4883431a7e5bcf4087bd90c8e6676
Damian Czajkowski <damian.czajkowski@mirumee.com>
2023-10-05 15:14:19 +0200

    add-and-edit-todo

--- src/todoapp/routers/todos.py
+++ src/todoapp/routers/todos.py
@@ -1,12 +1,12 @@
 import sys
 
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 
 sys.path.append("..")
 
 from typing import Optional
 
-from fastapi import APIRouter, Depends, HTTPException, Request
+from fastapi import APIRouter, Depends, HTTPException, Request, Form, status
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
@@ -34,15 +34,58 @@ def get_db():
 
 
 @router.get("/", response_class=HTMLResponse)
-async def read_all_by_user(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+async def read_all_by_user(request: Request, db: Session = Depends(get_db)):
+    todos = db.query(models.Todos).filter(models.Todos.owner_id == 1).all()
+    return templates.TemplateResponse("home.html", {"request": request, "todos": todos})
 
 
 @router.get("/add-todo", response_class=HTMLResponse)
-async def add_new_todo(request: Request):
+async def add_new_todo_page(request: Request):
     return templates.TemplateResponse("add-todo.html", {"request": request})
 
 
+@router.post("/add-todo", response_class=HTMLResponse)
+async def add_new_todo(
+    request: Request,
+    title: str = Form(...),
+    description: str = Form(...),
+    priority: int = Form(...),
+    db: Session = Depends(get_db),
+):
+    todo_model = models.Todos()
+    todo_model.title = title
+    todo_model.description = description
+    todo_model.priority = priority
+    todo_model.complete = False
+    todo_model.owner_id = 1
+
+    db.add(todo_model)
+    db.commit()
+    return RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
+
+
 @router.get("/edit-todo/{todo_id}", response_class=HTMLResponse)
-async def edit_todo(request: Request):
-    return templates.TemplateResponse("edit-todo.html", {"request": request})
+async def edit_todo(request: Request, todo_id: int, db: Session = Depends(get_db)):
+    todo = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+    return templates.TemplateResponse(
+        "edit-todo.html", {"request": request, "todo": todo}
+    )
+
+
+@router.post("/edit-todo/{todo_id}", response_class=HTMLResponse)
+async def edit_todo_commit(
+    request: Request,
+    todo_id: int,
+    title: str = Form(...),
+    description: str = Form(...),
+    priority: int = Form(...),
+    db: Session = Depends(get_db),
+):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+    todo_model.title = title
+    todo_model.description = description
+    todo_model.priority = priority
+
+    db.add(todo_model)
+    db.commit()
+    return RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
